300d4d55b6dfb9eea2c42a1042340f932b3a7437
Ricardo Dias <96152954+ricardond@users.noreply.github.com>
2023-04-16 16:06:53 -0300

    Atualização do php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,76 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// Compo E-mail
+$arquivo = "
+<style type='text/css'>
+body {
+margin:0px;
+font-family:Verdane;
+font-size:12px;
+color: #666666;
+}
+a{
+color: #666666;
+text-decoration: none;
+}
+a:hover {
+color: #FF0000;
+text-decoration: none;
+}
+</style>
+  <html>
+      <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+          <tr>
+            <td>
+<tr>
+               <td width='500'>Nome:$name</td>
+              </tr>
+              <tr>
+                <td width='320'>E-mail:<b>$email</b></td>
+   </tr>
+   <tr>
+                <td width='320'>Assunto:$subject</td>
+              </tr>
+              <tr>
+                <td width='320'>Mensagem:$message</td>
+              </tr>
+          </td>
+        </tr>
+        <tr>
+          <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
+        </tr>
+      </table>
+  </html>
+";
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'ricardond2021@gmail.com';
+//Variáveis
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+$nome = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$mensagem = $_POST['message'];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+
+
+//enviar
+
+  // emails para quem será enviado o formulário
+  $emailenviar = "ricardo.nunes@aluno.ifsertao-pe.edu.br";
+  $destino = $emailenviar;
+  $assunto = "Contato pelo Site";
+
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
+
+  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+  if($enviaremail){
+  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  $mgm = "ERRO AO ENVIAR E-MAIL!";
+  echo "";
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
